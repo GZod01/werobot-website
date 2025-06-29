c884e7e8df1b0a2778ec82fbcf794346b0831aa6
GZod01 <gzod01@gzod01.fr>
2025-06-29 13:52:37 +0200

    hello

--- static/js/cuttext.js
+++ static/js/cuttext.js
@@ -1,3 +1,16 @@
-document.querySelectorAll("*[cuttext]").forEach(a=>{
-    console.log(a);
-});
+function recurseCutText(el){
+    el.childNodes.forEach(node=>{
+	if(node.nodeType === 3 && node.nodeValue !== ""){
+	    console.log(node.nodeValue.trim()); 
+	}
+	else{
+	    recurseCutText(node);
+	}
+    })
+}
+function cuttext(){
+    document.querySelectorAll("*[cuttext]").forEach(a=>{
+	recurseCutText(a);
+    });
+}
+cuttext();
